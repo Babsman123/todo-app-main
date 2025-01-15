19caab9b24578955dd31edce896ac440b0fdda90
TundeAdelani <babspropertyconsult@gmail.com>
2025-01-15 18:25:12 +0100

    new commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const toggleBtn = [...document.querySelectorAll(".toggle-btn")];
 const container = document.querySelector(".container");
 
@@ -24,23 +26,41 @@ toggleBtn.forEach((btn) => {
 const inputTask = document.querySelector(".create-task");
 const addTaskBtn = document.querySelector(".btn-task");
 const taskContainer = document.querySelector(".list-container");
+const taskNumber = document.querySelector(".number");
+const clearCompletedBtn = document.querySelector(".clear-completed");
+
+//FOR deSKTOP DESIGN
+const allBtn = document.querySelector(".all-task");
+const activeBtn = document.querySelector(".active-task");
 const completeBtn = document.querySelector(".completed-task");
+
+//FOR MOBILE DESIGN
+const allBtnMobile = document.querySelector(".all-task-mobile");
+const activeBtnMobile = document.querySelector(".active-task-mobile");
+const completeBtnMobile = document.querySelector(".completed-task-mobile");
 
 let currentID = 1;
 let tasks = [];
 
+const udateTaskCount = function () {
+  taskNumber.textContent = tasks.length;
+};
 const updateTask = function () {
   taskContainer.textContent = "";
 
   const mapText = tasks.map((arrText) => arrText.text);
 
   mapText.forEach((arrText, index) => {
-    const html = `<li class="list-item">
-    <span class="btn-list"></span>
-    <p>
-      ${arrText}
-    </p>
-  </li>`;
+    const html = ` <li class="list-item">
+                <div class="list-item--task">
+                  <span class="btn-list"></span>
+                  <p>
+                    ${arrText}
+                  </p>
+                </div>
+
+                <img src="./images/icon-cross.svg" alt="icon-cross">
+              </li>`;
 
     taskContainer.insertAdjacentHTML("beforeend", html);
   });
@@ -59,6 +79,7 @@ const renderTask = function () {
   }
   inputTask.value = "";
   updateTask();
+  udateTaskCount();
 };
 
 const markTaskCompleted = function () {
@@ -66,26 +87,144 @@ const markTaskCompleted = function () {
   const taskCompletedBtn = [...document.querySelectorAll(".btn-list")];
 
   taskItems.forEach((task, index) => {
+    let isClicked = false;
+
     task.addEventListener("click", () => {
-      tasks[index].status = true;
-      taskCompletedBtn[index].classList.add("btn-list-change");
+      if (!isClicked) {
+        tasks[index].status = true;
+        taskNumber.textContent--;
+        taskCompletedBtn[index].classList.add("btn-list-change");
+        isClicked = true;
+        console.log(taskNumber.textContent);
+      } else {
+        tasks[index].status = false;
+        taskNumber.textContent++;
+        taskCompletedBtn[index].classList.remove("btn-list-change");
+        isClicked = false;
+        console.log(taskNumber.textContent);
+      }
     });
   });
 };
 
+const allTask = function () {
+  const allTheTask = tasks.filter((item) => item.id).map((item) => item.text);
+
+  taskContainer.textContent = "";
+
+  allTheTask.forEach((arrText, index) => {
+    const html = `<li class="list-item">
+      <span class="btn-list"></span>
+      <p>
+        ${arrText}
+      </p>
+    </li>`;
+
+    taskContainer.insertAdjacentHTML("beforeend", html);
+
+    if (tasks[index].status === true) {
+      completedTask();
+    }
+  });
+  if (tasks.length === 0) {
+    console.log("task is zeroe");
+    const emptyComplete = document.createElement("p");
+    emptyComplete.classList.add("enter-task");
+
+    emptyComplete.textContent = "No New Task Available";
+    taskContainer.appendChild(emptyComplete);
+  }
+  markTaskCompleted();
+};
+
+const activeTask = function () {
+  const actTask = tasks
+    .filter((item) => !item?.status)
+    .map((item) => item.text);
+
+  taskContainer.textContent = "";
+
+  if (actTask.length === 0) {
+    const emptyActive = document.createElement("p");
+    emptyActive.classList.add("enter-task");
+
+    emptyActive.textContent = "No Active Tasks";
+    taskContainer.appendChild(emptyActive);
+  } else {
+    actTask.forEach((arrText) => {
+      const html = `<li class="list-item">
+    <span class="btn-list"></span>
+    <p>
+      ${arrText}
+    </p>
+  </li>`;
+
+      taskContainer.insertAdjacentHTML("beforeend", html);
+    });
+  }
+  // completedTask();
+  markTaskCompleted();
+};
+
 const completedTask = function () {
-  const newTask = tasks.filter((item) => item?.status === true);
-  console.log(newTask);
-  tasks = newTask;
+  let completeTask = tasks
+    .filter((item) => item?.status)
+    .map((item) => item.text);
+
+  taskContainer.textContent = "";
+
+  if (completeTask.length === 0) {
+    const emptyComplete = document.createElement("p");
+    emptyComplete.classList.add("enter-task");
+
+    emptyComplete.textContent = "No Completed Tasks";
+    taskContainer.appendChild(emptyComplete);
+  } else {
+    completeTask.forEach((arrText, index) => {
+      const html = `<li class="list-item">
+  <span class="btn-list"></span>
+  <p>
+    ${arrText}
+  </p>
+</li>`;
+
+      taskContainer.insertAdjacentHTML("beforeend", html);
+    });
+
+    const taskCompletedBtn = [...document.querySelectorAll(".btn-list")];
+    taskCompletedBtn.forEach((item) => {
+      item.classList.add("btn-list-change");
+    });
+  }
+};
+
+const clearCompletedTask = function () {
+  let index = tasks.findIndex((item) => item.status);
+
+  tasks.splice(index, 1);
   updateTask();
 
-  const taskCompletedBtn = [...document.querySelectorAll(".btn-list")];
-  taskCompletedBtn.forEach((item) => {
-    item.classList.add("btn-list-change");
-  });
+  if (tasks.length === 0) {
+    const NewTask = document.createElement("p");
+    NewTask.classList.add("enter-task");
+    NewTask.textContent = `No Task Available`;
+
+    taskContainer.appendChild(NewTask);
+  } else {
+    updateTask();
+  }
 };
 
 //EVENT HANDLLERS
 addTaskBtn.addEventListener("click", renderTask);
+clearCompletedBtn.addEventListener("click", clearCompletedTask);
 
+//DESKTOP EVENT
+activeBtn.addEventListener("click", activeTask);
 completeBtn.addEventListener("click", completedTask);
+allBtn.addEventListener("click", allTask);
+
+//MOBILE EVENT HANDLERS
+activeBtnMobile.addEventListener("click", activeTask);
+completeBtnMobile.addEventListener("click", completedTask);
+allBtnMobile.addEventListener("click", allTask);
